5a3012d592988c0f996876adc2f735e9507d3173
Vladimir Panteleev <108007295+VPanteleev-S7@users.noreply.github.com>
2025-03-21 20:20:51 +0000

    vibe.data.serialization: Also respect policy attributes on types
    It is not always possible to specify policy attributes on fields, as
    we may want to control serialization/deserialization of values
    elsewhere than fields. Since there's no way to specify policy
    attributes via wrappers such as deserializeJson, allow specifying them
    on the type as well.

--- source/vibe/data/serialization.d
+++ source/vibe/data/serialization.d
@@ -368,11 +368,13 @@ private template serializeValueImpl(Serializer, alias Policy) {
 		void serializeValue(T, ATTRIBUTES...)(ref Serializer ser, auto ref T value) { serializeValueDeduced!(T, ATTRIBUTES)(ser, value); }
 	}
 
-	private void serializeValueDeduced(T, ATTRIBUTES...)(ref Serializer ser, auto ref T value)
+	private void serializeValueDeduced(T, FIELD_ATTRIBUTES...)(ref Serializer ser, auto ref T value)
 	{
 		import std.typecons : BitFlags, Nullable, Tuple, Typedef, TypedefType, tuple;
 
 		alias TU = Unqual!T;
+
+		alias ATTRIBUTES = AliasSeq!(FIELD_ATTRIBUTES, __traits(getAttributes, T));
 
 		alias Traits = .Traits!(TU, _Policy, ATTRIBUTES);
 
@@ -713,9 +715,11 @@ private template deserializeValueImpl(Serializer, alias Policy) {
 		return () @trusted { return cast(T)ret.move; } ();
 	}
 
-	T deserializeValueDeduced(T, ATTRIBUTES...)(ref Serializer ser) if(isMutable!T)
+	T deserializeValueDeduced(T, FIELD_ATTRIBUTES...)(ref Serializer ser) if(isMutable!T)
 	{
 		import std.typecons : BitFlags, Nullable, Typedef, TypedefType, Tuple;
+
+		alias ATTRIBUTES = AliasSeq!(FIELD_ATTRIBUTES, __traits(getAttributes, T));
 
 		alias Traits = .Traits!(T, _Policy, ATTRIBUTES);
 
@@ -2304,6 +2308,19 @@ unittest {
 
 	assertNotThrown(deserializeString("foobar"));
 	assertThrown!ConvException(deserializeString("baz"));
+}
+
+unittest {
+	@byName
+	enum Foo {
+		foobar
+	}
+
+	enum deserialized = Foo.foobar;
+	enum serialized = "V(Aya)(foobar)";
+
+	assert(serialize!TestSerializer(deserialized) == serialized);
+	assert(deserialize!(TestSerializer, Foo)(serialized) == deserialized);
 }
 
 unittest { // bit flag enums
